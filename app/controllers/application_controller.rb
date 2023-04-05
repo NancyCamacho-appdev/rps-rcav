d52da1d4357c0da70444341ea5a61ed9ab554e50
Nancy Camacho <nancyjc1297@gmail.com>
2023-04-05 22:27:28 +0000

    finished paper and rock routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,28 +8,27 @@ class ApplicationController < ActionController::Base
     render({ :template => "game_templates/rules.html.erb" })
   end
 
-  def play_paper 
-    render ({ :template => "game_templates/user_paper.html.erb"})
-
-    <h2>
-  They played <%= comp_move %>!
-</h2>
-
-<% if comp_move == "rock" %>
-  <h2> We tied! </h2>
-<% elsif comp_move == "paper" %>
- <h2> We lost! </h2>
-<% elsif comp_move == "scissors" %>
-  <h2> We won! </h2>
-<% end %>
-
-  end 
-
   def play_rock
-
     # write a ton of code
+
     #redirect_to("https://www.wikipedia.org")
 
+    # render({ :html => "<h1>Hello, world!</h1>".html_safe })
+
     render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render ({ :template => "game_templates/user_paper.html.erb" })
   end
 end
