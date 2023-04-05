8b730db0c2928e10d20cb2ae79d2ec7197086371
Nancy Camacho <nancyjc1297@gmail.com>
2023-04-05 21:21:27 +0000

    created paper and rocks routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,33 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
+
+  def play_paper 
+    render ({ :template => "game_templates/user_paper.html.erb"})
+
+    <h2>
+  They played <%= comp_move %>!
+</h2>
+
+<% if comp_move == "rock" %>
+  <h2> We tied! </h2>
+<% elsif comp_move == "paper" %>
+ <h2> We lost! </h2>
+<% elsif comp_move == "scissors" %>
+  <h2> We won! </h2>
+<% end %>
+
+  end 
+
   def play_rock
 
     # write a ton of code
     #redirect_to("https://www.wikipedia.org")
 
-    render({ :template => "game_templates/user_rock.html.erb"})
+    render({ :template => "game_templates/user_rock.html.erb" })
   end
 end
