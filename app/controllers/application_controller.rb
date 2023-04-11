10e8581a1c903a52ea2c0b6d4ae027b6a4005d38
Nancy Camacho <nancyjc1297@gmail.com>
2023-04-11 18:26:56 +0000

    finished rps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,35 @@ class ApplicationController < ActionController::Base
       @outcome = "lost"
     end
 
-    render ({ :template => "game_templates/user_paper.html.erb" })
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  ## @comp_move = ["rock", "paper", "scissors"].sample
+
+  ## if @comp_move == "rock"
+  # @outcome = "won"
+  #elsif @comp_move == "paper"
+  # @outcome = "tied"
+  # elsif @comp_move == "scissors"
+  #  @outcome = "lost"
+  # end
+  ##
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
+
+  def rules
+    render({ :template => "game_templates/rules.html.erb" })
   end
 end
